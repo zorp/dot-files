a65cc79507c987956218b3404f3e9583357e76f6
Rasmus Frey <rasmus@artz.dk>
2012-07-27 14:09:13 +0200

    Code cleanup

--- bin/git_update.sh
+++ bin/git_update.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 for file in *; do
-   if [ -d $file ]; then
-	if [ $file != 'contrib' ]; then
-	      cd $file; echo "Pulling git repro in" $file; git pull; cd ..;
-	fi
-   fi
+  if [ -d $file ]; then
+	  if [ $file != 'contrib' ]; then
+	    cd $file; echo "Pulling git repro in" $file; git pull; cd ..;
+	  fi
+  fi
 done
