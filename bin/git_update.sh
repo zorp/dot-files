1b79662877c16fbd0443c48c1946b5aead209331
Rasmus Frey <rasmus@artz.dk>
2012-08-15 09:27:33 +0200

    Added check for git repro

--- bin/git_update.sh
+++ bin/git_update.sh
@@ -3,7 +3,13 @@
 for file in *; do
   if [ -d $file ]; then
 	  if [ $file != 'contrib' ]; then
-	    cd $file; echo "Pulling git repro in" $file; git pull; cd ..;
+	    cd $file
+	    if ! git ls-files >& /dev/null; then
+        echo "NOT GIT - " $file "not in git"
+      else
+	      echo "Pulling git repro in" $file; git pull;
+	    fi
+	    cd ..;
 	  fi
   fi
 done
